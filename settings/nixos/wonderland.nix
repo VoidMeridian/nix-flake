14fd844f7feeec26630c7e7f122ae511cd6d4e47
Miss Vampira <voidmeridian@gmail.com>
2024-05-31 22:53:25 -0400

    update udev

--- settings/nixos/wonderland.nix
+++ settings/nixos/wonderland.nix
@@ -5,6 +5,6 @@
     enableUserService = true;
   };
   config.services.udev.extraRules = ''
-    KERNEL=="hidraw*", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0370", MODE="0760"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0370", MODE="0766"
   '';
 }
