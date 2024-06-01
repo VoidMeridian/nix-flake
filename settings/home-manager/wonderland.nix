3be5234854bbf1a9ecf9ba69319184ae7133bcef
Miss Vampira <voidmeridian@gmail.com>
2024-05-31 22:46:19 -0400

    commiting sins

--- settings/home-manager/wonderland.nix
+++ settings/home-manager/wonderland.nix
@@ -2,7 +2,8 @@
   imports = [../wonderland.nix];
   config.firefoxZoom = 1;
   config.home.activation = {
-    removeGtkrc = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    # just remove gtkrc-2.0 before nixos even checks for it
+    removeGtkrc = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
       run rm ~/.gtkrc-2.0 -rf
     '';
   };
