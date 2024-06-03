188a7dffb9440b6564ac80a28507ec6b9347a901
Miss Vampira <voidmeridian@gmail.com>
2024-06-03 18:40:12 -0400

    add os prober for wonderland, remove initialPassword

--- settings/nixos/wonderland.nix
+++ settings/nixos/wonderland.nix
@@ -7,5 +7,5 @@
   config.services.udev.extraRules = ''
     KERNEL=="hidraw*", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0370", MODE="0766"
   '';
-  config.users.users.alice.initialPassword = "nixos";
+  config.boot.loader.grub.useOSProber = true;
 }
