455068638844558042fbf882355bdab4eb1a6361
Miss Vampira <voidmeridian@gmail.com>
2024-06-19 19:49:48 -0400

    more betterdiscordflake testing

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,5 +1,9 @@
 {...}: {
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
-  services.blueman.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
 }
