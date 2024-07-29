4f0c686431d75f683e602427991b077342609dab
Miss Vampira <voidmeridian@gmail.com>
2024-07-29 11:35:08 -0400

    rust-rover

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -13,6 +13,7 @@
       export XDG_DATA_HOME="$HOME/.local/share"
           echo '\e[4 q'
 
+      alias rustrover="mkdir -p /tmp/rustrover; TMPDIR=/tmp/rustrover rust-rover > /dev/null 2>&1 &"
     '';
     initExtraFirst = ''
       DISABLE_MAGIC_FUNCTIONS=true
